66684684945481c829fa3013c76a7fcc22e31ac6
NOVA Openclaw <nova@dustintrammell.com>
2026-02-13 04:21:10 +0000

    chore: remove legacy clawdbot-patches (now running from source)

--- clawdbot-patches/hooks.js
+++
@@ -1,3 +0,0 @@
-export * from "./internal-hooks.js";
-export * from "./message-hooks.js";
-export { registerInternalHook as registerHook, unregisterInternalHook as unregisterHook, clearInternalHooks as clearHooks, getRegisteredEventKeys as getRegisteredHookEventKeys, triggerInternalHook as triggerHook, createInternalHookEvent as createHookEvent, } from "./internal-hooks.js";
